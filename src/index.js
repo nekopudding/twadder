82d6e92399538ec6fba7960bdf1a3951dd99e689
nekopudding <deany.y.ca@gmail.com>
2022-08-26 23:10:50 -0700

    init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +16,17 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBe_SUb0lkUXYNnu_XBZIKIGx1qoZ-1rZk",
+  authDomain: "twadder-b2796.firebaseapp.com",
+  projectId: "twadder-b2796",
+  storageBucket: "twadder-b2796.appspot.com",
+  messagingSenderId: "517933089324",
+  appId: "1:517933089324:web:166a26e6a55b09b7ff8789"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
