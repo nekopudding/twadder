87a1295909a4fe36b85a7dab190f4cf5977aa7b0
nekopudding <deany.y.ca@gmail.com>
2022-12-30 16:22:35 -0800

    create testaccount 2

--- frontend/src/pages/SignUp/SignUp.js
+++ frontend/src/pages/SignUp/SignUp.js
@@ -19,6 +19,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setToast } from 'app/toastSlice';
 import { baseURL, ins } from 'utils/fetch-api';
 
+const cred1 = {
+  username: 'dean',
+  password: 'yang1234'
+}
+const cred2 = {
+  username: 'dean2',
+  password: 'yang1234'
+}
+
 function SignUp() {
   const [signUpFormOpen, setSignUpFormOpen] = useState(false);
   const [signInFormOpen,setSignInFormOpen] = useState(false);
@@ -43,16 +52,12 @@ function SignUp() {
     dispatch(setToast({update: true, msg:''})); //reset toast msg
   },[])
 
-  const autoSignIn = async () => {
-    const formData = {
-      username: 'dean',
-      password: 'yang1234'
-    }
+  const autoSignIn = async (cred) => {
     try {
       const res = await ins({
         method: 'post',
         url: `${baseURL}/login`,
-        data: formData
+        data: cred
       });
       const {msg,sessionId} = res.data;
       if (msg) dispatch(setToast({update: !toast.update, msg: msg}));
@@ -63,6 +68,13 @@ function SignUp() {
       return dispatch(setToast({update: !toast.update, msg: err.response.data.msg || err.message}));
     }
   }
+  const testAccount1 = () => {
+    return autoSignIn(cred1);
+  }
+  const testAccount2 = () => {
+    return autoSignIn(cred2);
+  }
+
 
   return (
     <>
@@ -94,8 +106,11 @@ function SignUp() {
             <button className={`${styles.button} ${styles.outlined}`} onClick={signIn}>
               <span className='bodyHeader'>Sign in</span>
             </button>
-            <button className={`${styles.button} ${styles.light}`} onClick={autoSignIn}>
-              <span className='bodyHeader'>Sign in with test account</span>
+            <button className={`${styles.button} ${styles.light}`} onClick={testAccount1}>
+              <span className='bodyHeader'>Sign in with test account 1</span>
+            </button>
+            <button className={`${styles.button} ${styles.light}`} onClick={testAccount2}>
+              <span className='bodyHeader'>Sign in with test account 2</span>
             </button>
           </div>
         </div>
